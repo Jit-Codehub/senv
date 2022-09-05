c86e2be2bd6f70d5a264fb20425bc2f105416e79
Jitender Kumar <jkcodingjk@gmail.com>
2022-09-05 12:35:29 +0530

    testing needs to be done

--- webstory/jobs.py
+++ webstory/jobs.py
@@ -9,8 +9,8 @@ scheduler.add_jobstore(DjangoJobStore(), "default")
 @register_job(scheduler, "interval",hours=1002, replace_existing=True)
 def usa_today_scrapper_job():
     pinkvilla('pinkvilla-entertainment')
-    # pinkvilla('pinkvilla-fashion')
-    # pinkvilla('pinkvilla-lifestyle')
+    pinkvilla('pinkvilla-fashion')
+    pinkvilla('pinkvilla-lifestyle')
     # vogue()
     
 register_events(scheduler)
